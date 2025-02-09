27cfd3393f25aff7abf2416d132cc9f25c890914
thatboyreegan <reaganronald491@gmail.com>
2025-02-09 17:52:37 +0300

    chore: build app

--- alx-movie-app/next.config.mjs
+++ alx-movie-app/next.config.mjs
@@ -3,7 +3,9 @@ import withPWAInit from "@ducanh2912/next-pwa";
 /** @type {import('next').NextConfig} */
 
 const withPWA = withPWAInit({
-  dest: 'public'
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
 })
 
 const nextConfig = {
